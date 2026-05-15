4d47ca9f89de37d72c53e14b46216383d45133b5
Daniel Chicot <dan.chicot@gmail.com>
2026-05-15 14:15:32 +0100

    chore: initialize pnpm workspaces monorepo with TS, ESLint, Vitest, Prettier

---
+++ eslint.config.js
@@ -0,0 +1,20 @@
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    ignores: ['**/dist/**', '**/node_modules/**', '**/*.config.js'],
+  },
+  ...tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      '@typescript-eslint/consistent-type-imports': 'error',
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    },
+  },
+);

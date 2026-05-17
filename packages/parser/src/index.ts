153e62a0892255262b5bc289e96a37a8f9ef3f5c
Daniel Chicot <dan.chicot@gmail.com>
2026-05-17 13:15:11 +0100

    feat(parser): expose Phase 1 public API surface

---
+++ packages/parser/src/index.ts
@@ -0,0 +1,11 @@
+export { fetchHtml } from './http.js';
+export type { FetchHtmlOptions } from './http.js';
+
+export { detectPageType } from './page-type.js';
+export type { PageType } from './page-type.js';
+
+export { parseClubsDirectory } from './parse-clubs-directory.js';
+export { parseLeagueTable } from './parse-league-table.js';
+export type { LeagueTableRow } from './parse-league-table.js';
+export { parsePlayerRankings } from './parse-player-rankings.js';
+export type { PlayerRankingRow } from './parse-player-rankings.js';
